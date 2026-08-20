41f42082ef0d07980ca97c16d72b21e40c9f2e35
Shubham Pahurkar <shubham.pahurkar@outlook.com>
2026-08-21 00:15:46 +0530

    Initial commit: Spring Boot GraphQL hibrenate with postgres setup

---
+++ src/main/java/com/banking/apex_volt_banking_graphql_hibernate/ApexVoltBankingGraphqlHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.banking.apex_volt_banking_graphql_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexVoltBankingGraphqlHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexVoltBankingGraphqlHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/apex_volt_banking_graphql_hibernate/ApexVoltBankingGraphqlHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.apex_volt_banking_graphql_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexVoltBankingGraphqlHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
